1fc9df7ceabde98e3775ade2a16aaac5612a2182
Syeda Jannatus Saba <39720940+saba-phoenix@users.noreply.github.com>
2022-04-21 02:13:46 +0600

    Add the primary version of code

---
+++ app.js
@@ -0,0 +1,84 @@
+#!/usr/bin/env node
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const NodeGit = require("nodegit");
+const pathToRepo = require("path").resolve(".git");
+const LINEAR_API_KEY = ""; // write your linear API key here
+const LINEAR_TEAM_IDENTIFIER = ""; // write your linear team identifier here, Three letter word, all capital
+const { LinearClient, LinearFetch, User } = require("@linear/sdk");
+const getTodoFromComment = (comment) => __awaiter(void 0, void 0, void 0, function* () {
+    if (comment.substring(0, 4).toLowerCase() == "todo") {
+        comment = comment.substring(4).trim();
+        if (comment[0] === ":") {
+            comment = comment.substring(1).trim();
+            return comment;
+        }
+    }
+    return "";
+});
+const getTodoList = (pathToRepo) => __awaiter(void 0, void 0, void 0, function* () {
+    const todoList = [];
+    const repository = yield NodeGit.Repository.open(pathToRepo);
+    const commit = yield repository.getHeadCommit();
+    const tree = yield (commit ? commit.getTree() : null);
+    const index = yield repository.index();
+    const diff = yield NodeGit.Diff.treeToIndex(repository, tree, index, null);
+    const arrayConvenientPatch = yield diff.patches();
+    for (const patch of arrayConvenientPatch) {
+        const hunks = yield patch.hunks();
+        for (const hunk of hunks) {
+            const lines = yield hunk.lines();
+            for (const line of lines) {
+                const refinedLine = String.fromCharCode(line.origin()) + line.content().trim();
+                if (refinedLine === undefined)
+                    continue;
+                if (refinedLine.substring(0, 3) === "+//") {
+                    const todo = yield getTodoFromComment(refinedLine.substring(3).trim());
+                    if (todo !== "") {
+                        todoList.push(todo);
+                    }
+                }
+            }
+        }
+    }
+    return todoList;
+});
+// Call start
+(() => __awaiter(void 0, void 0, void 0, function* () {
+    const todoList = yield getTodoList(pathToRepo);
+    const linearClient = new LinearClient({ apiKey: LINEAR_API_KEY });
+    const teams = yield linearClient.teams();
+    let KEY = process.argv.slice(2)[0] !== undefined
+        ? process.argv.slice(2)[0]
+        : LINEAR_TEAM_IDENTIFIER;
+    let idx = -1, i = 0;
+    for (const team of teams.nodes) {
+        if (team.key === KEY) {
+            idx = i;
+            break;
+        }
+        i++;
+    }
+    if (idx === -1)
+        return 0;
+    const team = teams.nodes[idx];
+    if (team.id) {
+        for (const todo of todoList) {
+            console.log(todo);
+            yield linearClient.issueCreate({
+                teamId: team.id,
+                title: todo,
+            });
+        }
+    }
+    return 0;
+}))();
---
+++ app.ts
@@ -0,0 +1,81 @@
+#!/usr/bin/env node
+const NodeGit = require("nodegit");
+const pathToRepo = require("path").resolve(".git");
+const LINEAR_API_KEY = ""; // write your linear API key here
+const LINEAR_TEAM_IDENTIFIER = ""; // write your linear team identifier here, Three letter word, all capital
+const { LinearClient, LinearFetch, User } = require("@linear/sdk");
+
+const getTodoFromComment = async (comment: string) => {
+  if (comment.substring(0, 4).toLowerCase() == "todo") {
+    comment = comment.substring(4).trim();
+    if (comment[0] === ":") {
+      comment = comment.substring(1).trim();
+      return comment;
+    }
+  }
+  return "";
+};
+
+const getTodoList = async (pathToRepo: string) => {
+  const todoList: Array<string> = [];
+  const repository = await NodeGit.Repository.open(pathToRepo);
+  const commit = await repository.getHeadCommit();
+  const tree = await (commit ? commit.getTree() : null);
+  const index = await repository.index();
+  const diff = await NodeGit.Diff.treeToIndex(repository, tree, index, null);
+
+  const arrayConvenientPatch = await diff.patches();
+  for (const patch of arrayConvenientPatch) {
+    const hunks = await patch.hunks();
+    for (const hunk of hunks) {
+      const lines = await hunk.lines();
+      for (const line of lines) {
+        const refinedLine =
+          String.fromCharCode(line.origin()) + line.content().trim();
+        if (refinedLine === undefined) continue;
+        if (refinedLine.substring(0, 3) === "+//") {
+          const todo = await getTodoFromComment(
+            refinedLine.substring(3).trim()
+          );
+          if (todo !== "") {
+            todoList.push(todo);
+          }
+        }
+      }
+    }
+  }
+  return todoList;
+};
+
+// Call start
+(async () => {
+  const todoList = await getTodoList(pathToRepo);
+  const linearClient = new LinearClient({ apiKey: LINEAR_API_KEY });
+  const teams = await linearClient.teams();
+  let KEY =
+    process.argv.slice(2)[0] !== undefined
+      ? process.argv.slice(2)[0]
+      : LINEAR_TEAM_IDENTIFIER;
+  let idx = -1,
+    i = 0;
+  for (const team of teams.nodes) {
+    if (team.key === KEY) {
+      idx = i;
+      break;
+    }
+    i++;
+  }
+  if (idx === -1) return 0;
+  const team = teams.nodes[idx];
+  if (team.id) {
+    for (const todo of todoList) {
+      console.log(todo);
+      await linearClient.issueCreate({
+        teamId: team.id,
+        title: todo,
+      });
+    }
+  }
+  return 0;
+})();
+export {};
